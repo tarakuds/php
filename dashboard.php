d706e6d59e10acad50443b8bef2c0582fb202c58
tarakuds <37577346+tarakuds@users.noreply.github.com>
2020-04-09 22:54:09 +0100

    esit1

--- dashboard.php
+++ dashboard.php
@@ -16,5 +16,7 @@
    <h1>DASHBOARD</h1> 
    
    Welcome, <?php echo $_SESSION['fullname'] ?>, You are Logged in as: <b><?php echo $_SESSION['role']; ?></b> and your ID is <?php echo $_SESSION['loggedin'] ?>
+
+   <h3>Your Department is: <?php echo $_SESSION['dept'] ?></h3>
 </body>
 </html>
